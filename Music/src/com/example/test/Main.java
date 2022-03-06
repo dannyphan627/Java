0e173065b7391a2d31cca55c53b52c3312003ee8
dannyphan <dannyphan627@gmail.com>
2022-03-06 11:20:40 -0800

    Music Commit

---
+++ Music/src/com/example/test/Main.java
@@ -0,0 +1,16 @@
+package com.example.test;
+
+import com.example.test.model.Datasource;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Datasource datasource = new Datasource();
+        if(!datasource.open()){
+            System.out.println("Can't open datasource");
+            return;
+        }
+
+        datasource.close();
+    }
+}
